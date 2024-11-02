762ac3092f61d21c6cc4d668c52aa235c29e2838
faisal iqbal <faisaliqbal90909@gmail.com>
2024-11-02 08:51:55 +0000

    add register controller and bug fixes

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,94 +1,91 @@
-import mongoose from "mongoose";
-import jwt from "jsonwebtoken"
-import bcrypt from "bcrypt"
+import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
-const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true,
-        lowercase: true,
-        trim: true,
-        index: true  // use it for searching 
+const userSchema = new mongoose.Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+            index: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true, // corrected typo
+            trim: true,
+        },
+        fullName: {
+            type: String,
+            required: true,
+            trim: true,
+            index: true,
+        },
+        avatar: {
+            type: String, // cloudinary URL
+            required: true,
+        },
+        coverImage: {
+            type: String, // cloudinary URL
+        },
+        watchHistory: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Video",
+            },
+        ],
+        password: {
+            type: String,
+            required: [true, 'Password is required'],
+        },
+        refreshToken: {
+            type: String,
+        },
     },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        lowercase: true,
-        trim: true,
-    },
-    fullname: {
-        type: String,
-        required: true,
-        trim: true,
-        index: true  // use it for searching
-    },
-    avatar: {
-        type: String, // cloudinary url
-        required: true
-    },
-    coverImage: {
-        type: String, // cloudinary url
-    },
-    watchHistory: [
-        {
-        type: Schema.Types.ObjectId,
-        ref: "Video"
+    {
+        timestamps: true,
     }
-],
-password: {
-    type: String,
-    required: [true, "Password is required"]
-},
-refreshToken: {
-    type: String,
-    required: [true, "Password is required"]
-},
-refreshToken: {
-    type: String
-}
-
-},
-{
-    timestamps: true
-}
-)
+);
 
 userSchema.pre("save", async function (next) {
-    if (!this.isModified("password")) next();
-    this.password = await bcrypt.hash(this.password, 10)
-    next()
-})
+    if (!this.isModified("password")) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
 
-userSchema.methods.isPasswordCorrect = async function(password){
-    return await bcrypt.compare(password, this.password)
-}
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password);
+};
 
-userSchema.methods.generateAccessToken = function(){
-   return jwt.sign(
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign(
         {
             _id: this._id,
             email: this.email,
             username: this.username,
-            fullname: this.fullname
+            fullName: this.fullName,
         },
         process.env.ACCESS_TOKEN_SECRET,
         {
-            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
         }
-    )
-}
-userSchema.methods.generateRefreshToken = function(){
+    );
+};
+
+userSchema.methods.generateRefreshToken = function () {
     return jwt.sign(
         {
             _id: this._id,
         },
         process.env.REFRESH_TOKEN_SECRET,
         {
-            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
         }
-    )
-}
+    );
+};
 
 export const User = mongoose.model("User", userSchema);
